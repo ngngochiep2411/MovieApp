a0c18021fbc5bb5fec7e83e3d0e8e3fc7207b420
ngngochiep2411 <ngngochiep2411@gmail.com>
2025-03-31 06:59:13 +0700

    .

--- app/src/main/java/com/example/movieapp/Constant.kt
+++ app/src/main/java/com/example/movieapp/Constant.kt
@@ -2,7 +2,7 @@ package com.example.movieapp
 
 object Constant {
 
-    val BASE_URL = "https://efce-116-96-47-114.ngrok-free.app"
+    val BASE_URL = "https://2c7f-116-96-47-63.ngrok-free.app"
     val BASE_API_URL = "${BASE_URL}/api/"
     val BASE_IMAGE_URL = "${BASE_URL}/"
 }
